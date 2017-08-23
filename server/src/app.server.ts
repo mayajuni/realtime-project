2d3892d979c0cf988006a4ce56268af079d27d8c
dongjun.kwon <mayajuni10@gmail.com>
2017-08-23 17:02:33 +0900

    에러 처리 추가.
    - 공통 라우터 에러 처리.(promise로 묶고 catch로 처리)
    - eventEmitter.on('error') 추가.
    - NotFoundError, NullPointError 추가
    - onmassege에서 event가 없을시, 선언된 route가 없을시 에러 처리
    
    #5

--- server/src/app.server.ts
+++ server/src/app.server.ts
@@ -2,12 +2,14 @@ import { Server } from 'uws';
 import { LoggerModule } from './modules/logger.module';
 import InitDB from './db';
 import { WebSocket } from './types/websocket';
-import { eventEmitter } from './modules/events.module';
+import { checkRoute, eventEmitter } from './modules/events.module';
+import { NullPointError } from './modules/error.module';
 
 export default class AppServer {
     wss: Server;
 
-    constructor() {}
+    constructor() {
+    }
 
     /**
      * 1. RethinkDb connection
@@ -26,6 +28,8 @@ export default class AppServer {
         LoggerModule.log(`WebSocket listening on port ${port}`);
 
         this.connection();
+
+        this.onError();
     }
 
     private connection() {
@@ -43,23 +47,43 @@ export default class AppServer {
 
     private onMessage(ws: WebSocket) {
         ws.onmessage = event => {
-            if (event.data === null || event.data.size === 0) {
-                return;
+            try {
+                if (event.data === null || event.data.byteLength === 0) {
+                    throw new NullPointError(`socket onmessage don't have data.`);
+                }
+
+                const params = JSON.parse(event.data);
+                const route = params.route;
+                const action = params.action;
+                const payload = params.payload;
+
+                // 라우터가 있는지 체크 한다. 없으면 not found route 라는 에러가 난다.
+                checkRoute(route);
+
+                // ws.send 같은경우 data를 보낼 때 String, Blob, ArrayBuffer만 보낼 수 있기 때문에 JSON.stringify으로 묶어 준다.
+                const send = (route: string, action: string, payload: any): void => {
+                    ws.send(JSON.stringify({route, action, payload}));
+                };
+
+                eventEmitter.emit(route, {action, payload, ws, send});
+
+                LoggerModule.log(`socket call event(${action})`);
+            } catch (error) {
+                eventEmitter.emit('error', error, ws);
             }
-            const params = JSON.parse(event.data);
-            const route = params.route;
-            const action = params.action;
-            const payload = params.payload;
-
-            eventEmitter.emit(route, {action, payload, ws});
-
-            LoggerModule.log(`[syncro] socket call event(${action})`);
         };
     }
 
     private onClose(ws: WebSocket) {
         ws.onclose = event => {
-            LoggerModule.log(`[syncro] socket disconnected(${ws.ip})`);
+            LoggerModule.log(`socket disconnected(${ws.ip})`);
         };
+    }
+
+    private onError() {
+        eventEmitter.on('error', (error, ws) => {
+            LoggerModule.error(error);
+            ws.send(JSON.stringify({route: 'error', payload: {error}}));
+        });
     }
 }
--- server/src/decorators/router.decorator.ts
+++ server/src/decorators/router.decorator.ts
@@ -4,8 +4,13 @@ export function Router(name?: string) {
     return (target: any) => {
         const targetInstance = new target();
         const eventName = name || target.name;
-        eventEmitter.on(eventName, ({action, payload, ws}) => {
-            targetInstance[action]({payload, ws});
+        eventEmitter.on(eventName, ({action, payload, ws, send}) => {
+            // 에러 처리를 위해서 promise로 감싼다.
+            new Promise(() => {
+                targetInstance[action]({payload, ws, send});
+            }).catch(error => {
+                eventEmitter.emit('error', error, ws);
+            });
         });
     };
 }

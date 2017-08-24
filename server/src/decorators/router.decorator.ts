d3175687368a557081faa70c18802c94ae87381f
dongjun.kwon <mayajuni10@gmail.com>
2017-08-24 10:06:10 +0900

    eventName을 route로 변경처리

--- server/src/decorators/router.decorator.ts
+++ server/src/decorators/router.decorator.ts
@@ -3,8 +3,8 @@ import { eventEmitter } from '../modules/events.module';
 export function Router(name?: string) {
     return (target: any) => {
         const targetInstance = new target();
-        const eventName = name || target.name;
-        eventEmitter.on(eventName, ({action, payload, ws, send}) => {
+        const route = name || target.name;
+        eventEmitter.on(route, ({action, payload, ws, send}) => {
             // 에러 처리를 위해서 promise로 감싼다.
             new Promise(() => {
                 targetInstance[action]({payload, ws, send});

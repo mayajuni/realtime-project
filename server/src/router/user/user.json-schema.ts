84eae32941a34c730cb0fcc37c204fc8c8ac546d
dongjun.kwon <mayajuni10@gmail.com>
2017-08-30 14:34:52 +0900

    회원 가입 추가
    #12

--- server/src/router/user/user.json-schema.ts
+++ server/src/router/user/user.json-schema.ts
@@ -9,3 +9,57 @@ export const authorizeToken = {
     },
     required: ['token'],
 };
+
+export const register = {
+    description: '회원가입',
+    type: 'object',
+    properties: {
+        email: {
+            description: '회원 이메일',
+            type: 'string',
+            format: 'email',
+        },
+        name: {
+            description: '이름 혹은 닉네임',
+            type: 'string',
+        }
+    },
+    required: ['email', 'name'],
+    anyOf: [
+        {
+            properties: {
+                password: {
+                    description: '비밀번호',
+                    type: 'string',
+                },
+            },
+            required: ['password']
+        },
+        {
+            properties: {
+                kakaoId: {
+                    description: '카카오 고유 ID',
+                    type: 'string',
+                },
+            },
+            required: ['kakaoId']
+        },
+        {
+            properties: {
+                facebookId: {
+                    description: '페이스북 고유 ID',
+                    type: 'string',
+                },
+            },
+            required: ['facebookId']
+        },
+        {
+            properties: {
+                googleId: {
+                    description: '구글 고유 ID',
+                    type: 'string',
+                },
+            },
+            required: ['googleId']
+        }]
+};
--- server/test/spec/router/user.spec.ts
+++ server/test/spec/router/user.spec.ts
@@ -3,6 +3,7 @@ import * as chai from 'chai';
 import { sign } from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 import { join } from 'path';
+import InitDB from '../../../src/db/index';
 
 chai.should();
 
@@ -26,43 +27,126 @@ describe('User Test', () => {
         };
     });
 
-    it('authorizeToken', done => {
-        const param = {test: 123};
-        const token = sign(param, process.env.JWT_KEY);
-        ws.send(JSON.stringify({route: 'User', action: 'authorizeToken', payload: {token: token}}));
-        ws.onmessage = (event: any) => {
-            const params = JSON.parse(event.data);
+    describe('authorizeToken Test', () => {
+        it('authorizeToken', done => {
+            const param = {test: 123};
+            const token = sign(param, process.env.JWT_KEY);
+            ws.send(JSON.stringify({route: 'User', action: 'authorizeToken', payload: {token: token}}));
+            ws.onmessage = (event: any) => {
+                const params = JSON.parse(event.data);
 
-            params.route.should.equal('user');
-            params.action.should.equal('setUserInfo');
-            params.payload.test.should.equal(param.test);
-            done();
-        };
+                params.route.should.equal('user');
+                params.action.should.equal('setUserInfo');
+                params.payload.user.test.should.equal(param.test);
+                params.payload.token.should.equal(token);
+                done();
+            };
+        });
+
+        it('authorizeToken validation check', done => {
+            const param = {test: 123};
+            const token = sign(param, process.env.JWT_KEY);
+            ws.send(JSON.stringify({route: 'User', action: 'authorizeToken', payload: {token2: token}}));
+            ws.onmessage = (event: any) => {
+                const params = JSON.parse(event.data);
+
+                params.route.should.equal('error');
+                params.payload.error.name.should.equal('validation');
+                params.payload.error.code.should.equal(422);
+                done();
+            };
+        });
+
+        it('authorizeToken jwt key check', done => {
+            const param = {test: 123};
+            const token = sign(param, 'test');
+            ws.send(JSON.stringify({route: 'User', action: 'authorizeToken', payload: {token: token}}));
+            ws.onmessage = (event: any) => {
+                const params = JSON.parse(event.data);
+
+                params.route.should.equal('error');
+                params.payload.error.name.should.equal('authorize');
+                params.payload.error.code.should.equal(401);
+                done();
+            };
+        });
     });
 
-    it('authorizeToken validation check', done => {
-        const param = {test: 123};
-        const token = sign(param, process.env.JWT_KEY);
-        ws.send(JSON.stringify({route: 'User', action: 'authorizeToken', payload: {token2: token}}));
-        ws.onmessage = (event: any) => {
-            const params = JSON.parse(event.data);
+    describe('register Test', () => {
+        it('register validation check', done => {
+            const payload = {
+                password: 'test',
+                kakaoId: '3242',
+                facebookId: '3242',
+                googleId: '3242',
+            };
+            let index = 0;
 
-            params.route.should.equal('error');
-            params.payload.error.name.should.equal('ValidationError');
-            done();
-        };
-    });
+            ws.send(JSON.stringify({route: 'User', action: 'register', payload}));
+            ws.onmessage = (event: any) => {
+                const params = JSON.parse(event.data);
 
-    it('authorizeToken jwt key check', done => {
-        const param = {test: 123};
-        const token = sign(param, 'test');
-        ws.send(JSON.stringify({route: 'User', action: 'authorizeToken', payload: {token: token}}));
-        ws.onmessage = (event: any) => {
-            const params = JSON.parse(event.data);
+                params.route.should.equal('error');
+                params.payload.error.name.should.equal('validation');
+                params.payload.error.code.should.equal(422);
+                if (index < 1) {
+                    ws.send(JSON.stringify({
+                        route: 'User',
+                        action: 'register',
+                        payload: {email: 'test@test.com', name: 'test'}
+                    }));
+                    index++;
+                } else {
+                    done();
+                }
+            };
+        });
 
-            params.route.should.equal('error');
-            params.payload.error.name.should.equal('JsonWebTokenError');
-            done();
-        };
+        it('register', (done) => {
+            const payload = {
+                email: 'test@test.com',
+                name: 'test',
+                password: 'test',
+                kakaoId: '3242',
+                facebookId: '3242',
+                googleId: '3242',
+            };
+
+            ws.send(JSON.stringify({route: 'User', action: 'register', payload}));
+            ws.onmessage = (event: any) => {
+                const params = JSON.parse(event.data);
+
+                params.route.should.equal('user');
+                params.action.should.equal('setUserInfo');
+                params.payload.user.email.should.equal(payload.email);
+                params.payload.user.name.should.equal(payload.name);
+                params.payload.should.haveOwnProperty('token');
+                done();
+            };
+        });
+
+        it('register 중복 체크', (done) => {
+            const payload = {
+                email: 'test@test.com',
+                name: 'test',
+                password: 'test',
+            };
+
+            ws.send(JSON.stringify({route: 'User', action: 'register', payload}));
+            ws.onmessage = (event: any) => {
+                const params = JSON.parse(event.data);
+
+                params.route.should.equal('error');
+                params.payload.error.name.should.equal('register');
+                params.payload.error.code.should.equal(422);
+                done();
+            };
+        });
+
+        after(async () => {
+            const db = new InitDB();
+            await db.connect();
+            db.r.table('users').delete().run();
+        });
     });
 });

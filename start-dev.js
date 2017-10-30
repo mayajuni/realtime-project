1dd251ce57ab73fc88af2bf860375961d715d605
mayajuni <mayajuni10@gmail.com>
2017-10-30 10:20:31 +0900

    [server] 시작 부분 수정

--- start-dev.js
+++ start-dev.js
@@ -1,23 +1,22 @@
-const exec =  require('child_process').exec;
+const exec = require('child_process').exec;
 
 // server build
 const serverBuild = exec('cd server && npm start', (error) => {
-  if (error) {
-    throw new Error(error);
-  }
+    if (error) {
+        throw new Error(error);
+    }
 });
+serverBuild.stdout.pipe(process.stdout);
 
 // start server
-require('./server/dist/server').ready.then(() => {
-    // start vue
+
+// start vue
+setTimeout(() => {
     const vue = exec('cd vue && npm run dev', (error) => {
         if (error) {
             console.error(error);
         }
         server.close();
     });
-
     vue.stdout.pipe(process.stdout);
 });
-
-serverBuild.stdout.pipe(process.stdout);

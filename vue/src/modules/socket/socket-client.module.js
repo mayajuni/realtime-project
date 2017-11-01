b9707dc97a13ab3b5f857f590f214dbb767d7407
mayajuni <mayajuni10@gmail.com>
2017-11-01 17:18:44 +0900

    socket url 수정

--- vue/src/modules/socket/socket-client.module.js
+++ vue/src/modules/socket/socket-client.module.js
@@ -2,7 +2,8 @@ import eventEmiter from '../event-emiter/event-emiter.module'
 
 class SocketClient {
   constructor () {
-    this.url = `ws://${location.hostname}:3000`
+    const host = location.hostname === 'localhost' ? `${location.hostname}:3000` : 'socket.toclick.net'
+    this.url = `ws://${host}`
     this.reconnectDelay = [300, 1000, 5, 1.3]
     this.retries = this.reconnectDelay[2] || Infinity
     this.subscribes = new Set()
